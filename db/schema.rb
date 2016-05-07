17b4cd7b2590fb4e41e84386ff4a23e1b5ac0c21
JunGwon Kim <j464kim@uwaterloo.ca>
2016-05-06 23:12:58 -0400

    created controllers and models for Post and Comment & Defined hierarchial relationship between the two

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206163934) do
+ActiveRecord::Schema.define(version: 20160507022915) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.text     "msg"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -25,6 +32,12 @@ ActiveRecord::Schema.define(version: 20160206163934) do
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
